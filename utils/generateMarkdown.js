acbbc5b34cb33335c0dbd7a580d17b2dcc25908d
hlnicks <hlnicks@icloud.com>
2022-08-27 11:11:06 -0400

    corrected function to render license badge // completed function for rendering license link' '

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,16 @@
 // function that returns a license badge based on which license is passed in
 function renderLicenseBadge(license) {
   if (!license) {
-    return ' ';
+    return '';
   }
   if (license === 'MIT') {
-    return `
-    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-    `;
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
   } else if (license === 'ISC') {
-    return `
-    [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
-    `;
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)`;
   } else if (license === 'IBM') {
-    return `
-    [![License: IBM](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)
-    `;
+    return `[![License: IBM](https://img.shields.io/badge/License-IPL_1.0-blue.svg)`;
   } else {
-    return `
-    [![License: SIL](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)
-    `;
+    return `[![License: SIL](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)`;
   }
 }
 
@@ -26,7 +18,21 @@ function renderLicenseBadge(license) {
 // MIT', 'ISC', 'IBM', 'SIL
 
 // function that returns the license link
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+  if (license === 'MIT') {
+    return `[License: MIT](https://opensource.org/licenses/MIT)`;
+  } else if (license === 'ISC') {
+    return `
+    [License: ISC](https://opensource.org/licenses/ISC)`;
+  } else if (license === 'IBM') {
+    return `[License: IBM](https://opensource.org/licenses/IPL-1.0)`;
+  } else {
+    return `[License: SIL](https://opensource.org/licenses/OFL-1.1)`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
